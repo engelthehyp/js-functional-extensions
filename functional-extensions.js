690fea67b26e6879740c1c1947ba02d00510dadd
engelthehyp <engel03455@chuh.org>
2023-03-06 09:09:18 -0500

    Update functional-extensions.js
    Fixed my documentation wording in the Option, some and none components - there is no "pattern argument" anymore.

--- functional-extensions.js
+++ functional-extensions.js
@@ -445,8 +445,8 @@ function Option(item) {
  * The function that is applied may return 'some' or 'none', based on the processing done.
  * We need 'flatMap' because if we used map with an "Upward world-crossing" function,
  * we would end up with something like this: Option<Option<T>>. 'flatMap' "flattens" this into an Option<T>.
- *     - matchWith: Gets a regular value from the option by returning the result of the 'some' callback function
- * provided in the pattern argument. It can be considered as a "branch" - even though there is no explicit check on the value,
+ *     - matchWith: Gets a regular value from the option by returning the result of the 'some' callback function.
+ * It can be considered as a "branch" - even though there is no explicit check on the value,
  * polymorphism allows for the two different implementations of Option ('some' and 'none') to have different results.
  */
 function some(value) {
@@ -480,8 +480,8 @@ function some(value) {
  * Since the computation has already failed if you are working with 'none', the only reasonable course of action is to return 'none' also.
  *     - flatMap: Same as map, disregards the function provided and returns an instance of 'none'.
  * Since the computation has already failed if you are working with 'none', the only reasonable course of action is to return 'none' also.
- *     - matchWith: Gets a regular value from the option by returning the result of the 'none' callback function
- * provided in the pattern argument. Because there is no meaningful value in an instance of 'none', the 'none' callback receives no arguments.
+ *     - matchWith: Gets a regular value from the option by returning the result of the 'none' callback function.
+ * Because there is no meaningful value in an instance of 'none', the 'none' callback receives no arguments.
  * It can be considered as a "branch" - even though there is no explicit check on the value,
  * polymorphism allows for the two different implementations of Option ('some' and 'none') to have different results.
  */
